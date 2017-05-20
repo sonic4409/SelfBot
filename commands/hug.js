8f9b5e073e11a7553b0d948d36d9c8c5db765450
Dylan To <Dylan To>
2017-05-20 13:47:27 +0800

    more hug fixes

--- commands/hug.js
+++ commands/hug.js
@@ -1,11 +1,11 @@
 exports.run = (client, msg, date, Discord, args) => {
   //const images =
   let target = args[0];
-  if (target.length > 0) {
+  if (target.length > 2) {
     msg.edit(`:heart: ${client.user.username} hugs ${target}! :heart:`);
     console.log(`[${date}] Hug command was used successfully!`);
   } else {
-    msg.edit(":warning: Invalid Parameters!") .then(m => m.delete(1000));
+    msg.edit(":warning: Target name must be 3 characters or longer!") .then(m => m.delete(1000));
     console.log(`[${date}] Hug command was used improperly!`);
   }
 };
