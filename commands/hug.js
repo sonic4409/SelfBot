99c04a68ee7dde82f7e803ae26f0f8c9504e95a5
Dylan To <Dylan To>
2017-05-20 13:44:15 +0800

    fix hugs

--- commands/hug.js
+++ commands/hug.js
@@ -1,15 +1,9 @@
 exports.run = (client, msg, date, Discord, args) => {
   //const images =
   let target = args[0];
-  if (msg.mentions.members.length === 1) {
-    msg.edit(`:heart: <@${msg.author.id}> hugs ${target}! :heart:`);
+  if (target.length > 0) {
+    msg.edit(`:heart: ${client.user.username} hugs ${target}! :heart:`);
     console.log(`[${date}] Hug command was used successfully!`);
-  } else if (msg.mentions.members.length > 1) {
-    msg.edit(":warning: Too many arguments! Mentions one user only!");
-    console.log(`[${date}] Hug command was used improperly!`);
-  } else if (msg.mentions.members.length === 0) {
-    msg.edit(":warning: Mention one user!");
-    console.log(`[${date}] Hug command was used improperly!`);
   } else {
     msg.edit(":warning: Invalid Parameters!") .then(m => m.delete(1000));
     console.log(`[${date}] Hug command was used improperly!`);
