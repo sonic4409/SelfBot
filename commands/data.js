fbb7c572990a313f5e7a91c203a21f58667be819
GummiWummiBear <dylan.to@hotmail.com>
2017-05-29 17:42:38 +0800

    Create data.js

---
+++ commands/data.js
@@ -0,0 +1,12 @@
+exports.run = (client, msg, date, Discord, args) => {
+  const fs = require("fs"); //Load the filesystem module
+  const stats = fs.statSync("../db/deletedMessages.sql");
+  const fileSizeInBytes = stats.size;
+  //Convert the file size to megabytes (optional)
+  const fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
+  msg.channel.send({embed: {
+  color: 3447003,
+  description: `Current filesize of sqlite file is ${fileSizeInMegabytes}MB`
+  }});
+  console.log(`[${date}] File size of deletedMessages.sqlite was viewed!`);
+}
