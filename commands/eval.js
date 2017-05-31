7ceb04d95c362f4aca3fd12e04fa25c5997faf0b
GummiWummiBear <dylan.to@hotmail.com>
2017-05-31 17:37:08 +0800

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -19,7 +19,7 @@ exports.run = (client, msg, date, Discord, args, math, forecast) => { //Import e
       msg.edit(`:inbox_tray: **INPUT**\`\`\`js\n${code}\n\`\`\`\n:outbox_tray: **OUTPUT**\n\`\`\`js\n${cleanEval}\n\`\`\``);
       console.log(`[${date}] An eval command was used!`);
     } else if (args.length === 0) {
-      msg.edit(`:warning: No arguments were provided! :warning:`).then(m => m.delete(2000);
+      msg.edit(`:warning: No arguments were provided! :warning:`).then(m => m.delete(2000));
     }
   } catch (err) {
     msg.edit(`:inbox_tray: **INPUT**\`\`\`js\n${code}\n\`\`\`\n:outbox_tray: **OUTPUT**\n\`\`\`js\n${clean(err)}\n\`\`\``);
