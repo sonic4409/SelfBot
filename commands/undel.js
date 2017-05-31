9f2c9767ed3edd19198a6fa6f9dc929224152781
GummiWummiBear <dylan.to@hotmail.com>
2017-05-31 17:16:51 +0800

    Update undel.js

--- commands/undel.js
+++ commands/undel.js
@@ -10,7 +10,7 @@ exports.run = (client, msg, date, Discord, args, math, forecast, sql) => {
         .setTitle(":recycle: Most Recent Deleted Message! :recycle:")
         .setFooter(`${row.userId.tag} (${row.userId})`, row.userId.displayAvatarURL)
         .addField("Message:", `"${row.msgContent}"`);
-      msg.channel.send({embed: embed});
+      msg.channel.send({embed: embed}).catch(err => console.log(err));
       //msg.channel.send(`**(${row.userId}) Deleted Message**\n"${row.msgContent}"`);
       console.log(`[${date}] Undel command was used!`);
     }
