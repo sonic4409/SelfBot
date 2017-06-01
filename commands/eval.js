c135f2772829046c77ebdc03727ec2ed481d3c52
GummiWummiBear <dylan.to@hotmail.com>
2017-06-01 17:07:35 +0800

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,14 +1,12 @@
 exports.run = (client, msg, date, Discord, args, math, forecast) => { //Import everything for all commands and stuff
-
   function clean(text) {
     if (typeof(text) === "string")
       return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
     else
         return text;
   }
-
+  var code = args.join(" ");
   try {
-    var code = args.join(" ");
     var evaled = eval(code);
 
     if (typeof evaled !== "string")
@@ -23,6 +21,6 @@ exports.run = (client, msg, date, Discord, args, math, forecast) => { //Import e
     }
   } catch (err) {
     msg.edit(`:inbox_tray: **INPUT**\`\`\`js\n${code}\n\`\`\`\n:outbox_tray: **OUTPUT**\n\`\`\`js\n${clean(err)}\n\`\`\``);
-    console.log(`[${date}] Eval command failed!`);
+    console.log(`[${date}] Eval command failed!\nERROR:\n${err}`);
   }
 };
