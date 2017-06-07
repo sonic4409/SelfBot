0d06cea9f41f01563832c3eacb23adfd7d24bfa4
GummiWummiBear <dylan.to@hotmail.com>
2017-06-07 19:58:41 +0800

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -3,7 +3,7 @@ exports.run = (client, msg, date, Discord) => {
     .setColor(0x7CFC00)
     .setTitle("Stats:")
     .setFooter(`Node version: ${process.version} | Discord.js version: ${Discord.version}`, client.user.displayAvatarURL)
-    .setTimestamp
+    .setTimestamp()
     .addField("Memory", `${(process.memoryUsage().heapUsed/1000000).toFixed(2)}MB`, true)
     .addField("Uptime", `≈ ${Math.round(process.uptime()/60)} minutes`, true);
 
