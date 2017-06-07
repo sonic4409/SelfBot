fd0c23a676c50899732631891753b0e4d84e1f80
GummiWummiBear <dylan.to@hotmail.com>
2017-06-07 19:45:41 +0800

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -2,8 +2,10 @@ exports.run = (client, msg, date, Discord) => {
   const embed = new Discord.RichEmbed()
     .setColor(0x7CFC00)
     .setTitle("Stats:")
+    .setFooter(`Node version: ${process.version}`, `Discord.js version: ${Discord.version}`)
     .addField("Memory", `${(process.memoryUsage().heapUsed/1000000).toFixed(2)}MB`, true)
     .addField("Uptime", `≈ ${Math.round(process.uptime()/60)} minutes`, true);
+
   msg.delete();
   msg.channel.send({embed: embed});
 }
