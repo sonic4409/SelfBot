e7e54abe916687f5b4629b8b64fdc60e0d46e6e1
GummiWummiBear <dylan.to@hotmail.com>
2017-06-12 19:49:37 +0800

    Create read.js

--- commands/read.js
+++ commands/read.js
@@ -1,4 +1,5 @@
 exports.run = (client, msg, date, Discord) => {
-  msg.guild.acknowledge().then(() => msg.edit("Marked all channels in the guild as read!").then(m => m.delete(3000));
+  msg.guild.acknowledge();
+  msg.edit("Marked all channels in the guild as read!").then(m => m.delete(3000));
   console.log(`[${date}] Marked all channels in the guild as read!`);
 };
