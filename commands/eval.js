b9cd38c7fa7879192c00e4de5e49ee163049f501
GummiWummiBear <dylan.to@hotmail.com>
2017-06-12 16:28:59 +0800

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -11,8 +11,7 @@ exports.run = (client, msg, date, Discord, args, math, forecast, sql) => { //Imp
 
     if (typeof evaled !== "string")
       return evaled = require("util").inspect(evaled);
-    
-    var cleanEval = clean(evaled.replace(new RegExp(client.token, "g"), "Nope")); //Remove token
+
     if (code.length > 0) {
       msg.edit(`:inbox_tray: **INPUT**\`\`\`js\n${code}\n\`\`\`\n:outbox_tray: **OUTPUT**\n\`\`\`js\n${cleanEval}\n\`\`\``).catch(err => console.log(err));
       console.log(`[${date}] An eval command was used!`);
