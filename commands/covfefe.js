808ab6ffeacc67d7d0aa29865ac364e1be5a6a1b
GummiWummiBear <dylan.to@hotmail.com>
2017-06-12 19:42:16 +0800

    Create covfefe.js

--- commands/covfefe.js
+++ commands/covfefe.js
@@ -1,8 +1,8 @@
 exports.run = (client, msg, date, Discord, args) => {
-  let covfefify = s=>([,a,b,c]=s.match`(.*?[aeiouy]+(.)).*?([aeiouy])`,a+(b=(a="bcdfgszkvtgp")[11-a.search(b)]||b)+c+b+c)
+  let covfefify = s=>([,a,b,c]=s.match`(.*?[aeiouy]+(.)).*?([aeiouy])`,a+(b=(a="bcdfgszkvtgp")[11-a.search(b)]||b)+c+b+c);
   const embed = new Discord.RichEmbed()
     .setTitle("Covfefify a String!")
     .setDescription(`${args[0]} => ${covfefify(args[0])}`);
   msg.channel.send({embed: embed});
-  console.log(`[${date}] A string was Covfefified!`);
+  console.log(`[${date}] A word was Covfefified!`);
 }
