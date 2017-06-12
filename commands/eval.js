32ad9df692f3b43d072868844c6002ba86e524ea
GummiWummiBear <dylan.to@hotmail.com>
2017-06-12 10:59:54 +0800

    Create eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -6,14 +6,13 @@ exports.run = (client, msg, date, Discord, args, math, forecast, sql, clean) => 
     if (typeof evaled !== "string")
       return evaled = require("util").inspect(evaled);
     
-    var cleanEval = clean(evaled);
-    //var cleanEval = clean(evaled.replace(new RegExp(client.token, "g"), "Nope"));
+    var cleanEval = clean(evaled.replace(new RegExp(client.token, "g"), "Nope"));
     if (args.length > 0) {
-      msg.edit(`:inbox_tray: **INPUT**\`\`\`js\n${code}\n\`\`\`\n:outbox_tray: **OUTPUT**\n\`\`\`js\n${cleanEval}\n\`\`\``);
+      msg.edit(`:inbox_tray: **INPUT**\`\`\`js\n${code}\n\`\`\`\n:outbox_tray: **OUTPUT**\n\`\`\`js\n${cleanEval}\n\`\`\``).catch(err => console.log(err));
       console.log(`[${date}] An eval command was used!`);
     }
   } catch (err) {
-    msg.edit(`:inbox_tray: **INPUT**\`\`\`js\n${code}\n\`\`\`\n:outbox_tray: **OUTPUT**\n\`\`\`js\n${clean(err)}\n\`\`\``);
+    msg.edit(`:inbox_tray: **INPUT**\`\`\`js\n${code}\n\`\`\`\n:outbox_tray: **OUTPUT**\n\`\`\`js\n${clean(err)}\n\`\`\``).catch(err => console.log(err));
     console.log(`[${date}] Eval command failed!\nERROR:\n${err}`);
   }
 };
