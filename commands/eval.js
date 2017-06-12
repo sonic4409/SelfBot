bfd982caa4d5b0442b4fb5b65eb6ba501c9fb6ea
GummiWummiBear <dylan.to@hotmail.com>
2017-06-12 11:03:11 +0800

    Create eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -7,7 +7,7 @@ exports.run = (client, msg, date, Discord, args, math, forecast, sql, clean) => 
       return evaled = require("util").inspect(evaled);
     
     var cleanEval = clean(evaled.replace(new RegExp(client.token, "g"), "Nope"));
-    if (args.length > 0) {
+    if (code.length > 0) {
       msg.edit(`:inbox_tray: **INPUT**\`\`\`js\n${code}\n\`\`\`\n:outbox_tray: **OUTPUT**\n\`\`\`js\n${cleanEval}\n\`\`\``).catch(err => console.log(err));
       console.log(`[${date}] An eval command was used!`);
     }
