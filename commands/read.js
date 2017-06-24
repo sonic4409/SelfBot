87ac1ed7c4e4dc517b937bf030b19683e416582f
GummiWummiBear <dylan.to@hotmail.com>
2017-06-24 21:35:37 +0800

    Update read.js

--- commands/read.js
+++ commands/read.js
@@ -1,5 +1,11 @@
-exports.run = (client, msg, date, Discord) => {
-  msg.guild.acknowledge();
-  msg.edit("Marked all channels in the guild as read!").then(m => m.delete(3000));
-  console.log(`[${date}] Marked all channels in the guild as read!`);
+exports.run = (client, msg, date, Discord, args) => {
+  if (args[0] === "all") {
+    client.guilds.forEach(g => g.acknowledge());
+     msg.edit(`[${date}] Marked all guilds as read!`).then(m => m.delete(2000));
+  } else {
+    msg.guild.acknowledge();
+    msg.edit("Marked all channels in the guild as read!").then(m => m.delete(2000));
+    console.log(`[${date}] Marked all channels in the guild as read!`);
+  }
+
 };
