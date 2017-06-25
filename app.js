4a0eefa88f5fd67a4f51a87576b482ff4b9264b1
GummiWummiBear <dylan.to@hotmail.com>
2017-06-25 20:13:17 +0800

    Asyncify?

--- app.js
+++ app.js
@@ -39,13 +39,28 @@ client.on("message", msg => {
   }
 
 });
-client.on("messageDelete", msg => {
+client.on("messageDelete", async (msg) => {
   if (msg.author.bot) return; //Ignores bots' messages
   if (msg.author.id === client.user.id) return; //Ignore own messages
   if (msg.channel.type === "dm") return; //Ignores messages from DMs
   if (msg.length === 0) return;
-  if (msg.content.includes("\\")) return; //I'm incapable of escaping backslashes
-  
+
+  try {
+    const row = await sql.get(`SELECT * FROM deletedMessages WHERE channelId ='${msg.channel.id}'`);
+    if(!row) {
+      await sql.run("INSERT INTO deletedMessages (userId, channelId, msgContent) VALUES (?, ?, ?)", [msg.author.id, msg.channel.id, msg.content]);
+      console.log("Could not find the row, so created a new one for the channel!");
+    } else {
+      await sql.run("REPLACE INTO deletedMessages (userId, channelId, msgContent) VALUES(?, ?, ?)", [msg.author.id, msg.channel.id, msg.content]);
+      console.log("Updated the row!");
+    }
+  } catch (err) {
+    console.error;
+    await sql.run("CREATE TABLE IF NOT EXISTS deletedMessages (userId TEXT, channelId TEXT, msgContent TEXT)");
+    await sql.run("INSERT INTO deletedMessages (userId, channelId, msgContent) VALUES (?, ?, ?)", [msg.author.id, msg.channel.id, msg.content]);
+    console.log("Created Table!"); //Create Table for Deleted Messages
+  }
+  /*
   sql.get(`SELECT * FROM deletedMessages WHERE channelId ='${msg.channel.id}'`).then(row => {
     if(!row) {
       sql.run("INSERT INTO deletedMessages (userId, channelId, msgContent) VALUES (?, ?, ?)", [msg.author.id, msg.channel.id, msg.content]);
@@ -61,7 +76,7 @@ client.on("messageDelete", msg => {
       console.log("Created Table!");
     }); //Create Table for Deleted Messages
   });
-});
+  */
 });
 
 client.on("error", console.error);
