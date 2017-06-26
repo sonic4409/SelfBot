e58a2a223fad835edeb8e9a8a1a12e70605dc8d5
GummiWummiBear <dylan.to@hotmail.com>
2017-06-26 10:27:30 +0800

    Asynced it

--- commands/undel.js
+++ commands/undel.js
@@ -1,5 +1,6 @@
 exports.run = async (client, msg, date, Discord, args, math, forecast, sql) => {
-  sql.get(`SELECT * FROM deletedMessages WHERE channelId ='${msg.channel.id}' ORDER BY ROWID ASC LIMIT 1`).then(row => {
+  try {
+    const row = await sql.get(`SELECT * FROM deletedMessages WHERE channelId ='${msg.channel.id}' ORDER BY ROWID ASC LIMIT 1`)
     if (!row) {
       msg.edit(":warning: Could not find the row! :warning:").then(m => m.delete(2000));
       console.log(`[${date}] Could not find the row!`);
@@ -10,11 +11,11 @@ exports.run = async (client, msg, date, Discord, args, math, forecast, sql) => {
         .setFooter(`${msg.guild.members.get(row.userId).user.tag} (${row.userId})`, msg.guild.members.get(row.userId).user.displayAvatarURL)
         .addField("Message:", `"${row.msgContent}"`);
       msg.delete();
-      msg.channel.send({embed: embed}).catch(err => console.log(err));
+      msg.channel.send({embed: embed});
       console.log(`[${date}] Undel command was used!`);
     }
-  }).catch((err) => {
-    console.log(err);
-    sql.run("CREATE TABLE IF NOT EXISTS deletedMessages (userId TEXT, channelId TEXT, msgContent TEXT)");
-  });
+  } catch (err) {
+      console.log(err);
+      sql.run("CREATE TABLE IF NOT EXISTS deletedMessages (userId TEXT, channelId TEXT, msgContent TEXT)");
+  }
 };
