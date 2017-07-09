4cb23f454a1375c48e246abd2ae0eaf944ef6027
GummiWummiBear <dylan.to@hotmail.com>
2017-07-09 21:25:19 +0800

    I think I fixed it

--- commands/mock.js
+++ commands/mock.js
@@ -1,6 +1,6 @@
 exports.run = async (client, msg, date, Discord, args) => {
-  const mockify = async (input) => {
-    return await input.split("").map((value, index) => index % 2 ? value.toUpperCase() : value.toLowerCase()).join("");
+  const mockify = (input) => {
+    return input.split("").map((value, index) => index % 2 ? value.toUpperCase() : value.toLowerCase()).join("");
   };
   await msg.edit(mockify(args.join(" ")));
   console.log(mockify(`[${date}] Some text has been mockified!`));
