6b1bf860051ee8f432e85f11e0f87e5cf3073ec4
GummiWummiBear <dylan.to@hotmail.com>
2017-07-12 17:46:34 +0800

    FeelsGunMan

--- commands/covfefe.js
+++ commands/covfefe.js
@@ -1,7 +1,8 @@
 exports.run = async(client, msg, date, Discord, args) => {
-  function covfefify(text) {
-    ([, a, b, c] = text.match `(.*?[aeiouy]+(.)).*?([aeiouy])`, a + (b = (a = "bcdfgszkvtgp")[11 - a.search(b)] || b) + c + b + c);
-  }
+  let a = undefined;
+  let b = undefined;
+  let c = undefined;
+  const covfefify = string => ([undefined, a, b, c] = string.match`(.*?[aeiouy]+(.)).*?([aeiouy])`, a + (b = (a = "bcdfgszkvtgp") [11 - a.search(b)] || b) + c + b + c);
   const embed = await new Discord.RichEmbed()
     .setTitle("Covfefify a Word!")
     .setColor(3447003)
