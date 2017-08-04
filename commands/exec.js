a8990222ecfc1f343d0df6abd3cdad9328844215
GummiWummiBear <dylan.to@hotmail.com>
2017-08-04 17:55:27 +0800

    Indentation aaa low battery

--- commands/exec.js
+++ commands/exec.js
@@ -1,23 +1,23 @@
 const { exec } = require("child_process");
 exports.run = async (client, msg, date, args) => {
-  exec(args.join(" "), (error, stdout, stderr) {
-	  if (error) {
+	exec(args.join(" "), (error, stdout, stderr) {
+		if (error) {
 		  const m = await msg.edit("It failed ecks dee");
 			m.delete(3000);
 		  console.error(`[$date] Fail... \n${error}`);
 		} else {
-		  msg.edit(`:inbox_tray: **INPUT:**\n\`\`\`xl\n${args.join(" ")}\n\`\`\`:outbox_tray: **OUTPUT:**\n\`\`\`xl\n${client.clean(stdout)}\n\`\`\``);
+			msg.edit(`:inbox_tray: **INPUT:**\n\`\`\`xl\n${args.join(" ")}\n\`\`\`:outbox_tray: **OUTPUT:**\n\`\`\`xl\n${client.clean(stdout)}\n\`\`\``);
 		}
 	}
 };
 
 exports.conf = {
-  enabled: true,
+	enabled: true,
 	aliases: ["run", "e"]
 };
 
 exports.help = {
-  name: "exec",
+	name: "exec",
 	description: "Run code in the terminal",
 	usage: "/`exec [code]/`"
 };
