7192945f87fa1517e7f6e44bb6c42b8eda32bc08
GummiWummiBear <dylan.to@hotmail.com>
2017-08-04 18:19:52 +0800

    Wew

--- commands/exec.js
+++ commands/exec.js
@@ -2,13 +2,13 @@ const { exec } = require("child_process");
 exports.run = async (client, msg, date, args) => {
 	exec(args.join(" "), (error, stdout, stderr) {
 		if (error) {
-		  const m = await msg.edit("It failed ecks dee");
+			const m = await msg.edit("It failed ecks dee");
 			m.delete(3000);
-		  console.error(`[$date] Fail... \n${error}`);
+			console.error(`[$date] Fail... \n${error}`);
 		} else {
 			msg.edit(`:inbox_tray: **INPUT:**\n\`\`\`xl\n${args.join(" ")}\n\`\`\`:outbox_tray: **OUTPUT:**\n\`\`\`xl\n${client.clean(stdout)}\n\`\`\``);
 		}
-	}
+	});
 };
 
 exports.conf = {
