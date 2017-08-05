dd11cea4ca0d5ffaa3789a0307bfe078b678e034
GummiWummiBear <dylan.to@hotmail.com>
2017-08-05 11:11:20 +0800

    I'm too lazy to do it properly ecks dee

--- commands/exec.js
+++ commands/exec.js
@@ -1,12 +1,7 @@
 const { exec } = require("child_process");
 exports.run = (client, msg, args, date) => {
-	exec(args.join(" "), async(error, stdout, stderr) => {
-		if (error) {
-			const m = await msg.edit("It failed ecks dee");
-			m.delete(3000);
-			console.error(`[$date] Fail... \n${error}`);
-			return;
-		}
+	exec(args.join(" "), (error, stdout, stderr) => {
+		if (error) console.error(`[${date}] Fail... \n${error}\n${stderr}`);
 		msg.edit(`:inbox_tray: **INPUT:**\n\`\`\`xl\n${args.join(" ")}\n\`\`\`:outbox_tray: **OUTPUT:**\n\`\`\`xl\n${client.clean(stdout)}\n\`\`\``);
 		console.log(`[${date}] Success!`);
 	});
