4cd8cbdabdbb90573eabfdf523e222b41ae81ec8
GummiWummiBear <dylan.to@hotmail.com>
2017-08-05 11:16:35 +0800

    Update exec.js

--- commands/exec.js
+++ commands/exec.js
@@ -1,7 +1,11 @@
 const { exec } = require("child_process");
 exports.run = (client, msg, args, date) => {
-	exec(args.join(" "), (error, stdout, stderr) => {
-		if (error) console.error(`[${date}] Fail... \n${error}\n${stderr}`);
+	exec(args.join(" "), async(error, stdout, stderr) => {
+		if (error) {
+			const m = msg.edit(`It failed ecks dee:\n${stderr}`);
+			m.delete(5000);
+			console.error(`[${date}] Fail... \n${error}`);
+		}
 		msg.edit(`:inbox_tray: **INPUT:**\n\`\`\`xl\n${args.join(" ")}\n\`\`\`:outbox_tray: **OUTPUT:**\n\`\`\`xl\n${client.clean(stdout)}\n\`\`\``);
 		console.log(`[${date}] Success!`);
 	});
