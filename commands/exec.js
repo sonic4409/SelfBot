a3bc9d164235c73fa91867a70efed7cea0fca7b5
GummiWummiBear <dylan.to@hotmail.com>
2017-08-16 21:48:16 +0800

    Update exec.js

--- commands/exec.js
+++ commands/exec.js
@@ -1,15 +1,18 @@
 const { exec } = require("child_process");
 exports.run = (client, msg, args, date) => {
-	exec(args.join(" "), async(error, stdout, stderr) => {
-		if (error) {
-			const m = await msg.edit(`It failed ecks dee:\n${stderr}`);
-			m.delete(5000);
-			console.error(`[${date}] Fail... \n${error}`);
-			return;
-		}
-		msg.edit(`:inbox_tray: **INPUT:**\n\`\`\`xl\n${args.join(" ")}\n\`\`\`:outbox_tray: **OUTPUT:**\n\`\`\`xl\n${client.clean(stdout)}\n\`\`\``);
-		console.log(`[${date}] Success!`);
-	});
+  if (!args) {
+    const m = await msg.edit("**:warning: Nothing was provided to run! :warning:**");
+    m.delete(3000);
+  }
+  exec(args.join(" "), async(error, stdout, stderr) => {
+    if (error) {
+      msg.edit(`It failed ecks dee:\n${stderr}`);
+      console.error(`[${date}] Fail... \n${error}`);
+      return;
+    }
+    msg.edit(`:inbox_tray: **INPUT:**\n\`\`\`xl\n${args.join(" ")}\n\`\`\`:outbox_tray: **OUTPUT:**\n\`\`\`xl\n${client.clean(stdout)}\n\`\`\``);
+    console.log(`[${date}] Success!`);
+  });
 };
 
 exports.conf = {
