2c7a03a79c272e858747471c69caef7c8e48a9f8
GummiWummiBear <dylan.to@hotmail.com>
2017-08-23 09:51:26 +0800

    Clean up code I guess

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const Discord = require("discord.js");
+const { Client, Collection } = require("discord.js");
 
-const client = new Discord.Client();
+const client = new Client();
 
-const fs = require("fs");
 /*
 
 fs.writeFile("./config.json", JSON.stringify({
@@ -23,17 +22,15 @@ client.info = require("./package.json");
 
 console.log(`Starting SelfBot... (v${client.info.version})\nNode version: ${process.version}\nDiscord.js version: ${Discord.version}`);
 
-const Raven = require("raven");
-Raven.config(process.env.ravenDSN).install();
+require("raven").config(process.env.ravenDSN).install();
 
-const sql = require("sqlite");
-sql.open("./db/deletedMessages.sqlite");
+require("sqlite").open("./db/deletedMessages.sqlite");
 
 const { promisify } = require("util");
-const readdir = promisify(fs.readdir);
+const readdir = promisify(require("fs").readdir);
 
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
+client.commands = new Collection();
+client.aliases = new Collection();
 
 require("./modules/functions.js")(client);
 
