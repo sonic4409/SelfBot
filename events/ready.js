40ada59ce1103e64543fc205d59af1538a3cc418
GummiWummiBear <dylan.to@hotmail.com>
2017-08-28 10:49:25 +0800

    Disable line

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
   console.log(`Logged in as ${client.user.username}!`);
-  await wait(5000);
+  await wait(5000); // eslint-disable-line
   console.log(`Serving ${client.users.size} users!`);
 };
