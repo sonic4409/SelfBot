e5793b7aa6fad2ffb1d4afa69832837882c2bbee
GummiWummiBear <dylan.to@hotmail.com>
2017-09-04 17:40:37 +0800

    Delete ready.js

--- SelfBot-master/events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client) => {
-  console.log(`Logged in as ${client.user.username}!`);
-  await wait(5000); // eslint-disable-line
-  console.log(`Serving ${client.users.size} users!`);
-};
